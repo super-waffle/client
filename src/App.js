2fdf065945b72da5dbc6bc4a17432d31cd37bd61
rosieyeon <sseyeon956@gmail.com>
2022-01-25 15:29:24 +0900

    wip: router 이름 업데이트

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet, NavLink } from "react-router-dom";
 
 function App() {
   return (
@@ -6,16 +6,16 @@ function App() {
       <h1>공부하는 습관</h1>
       <nav
         style={{
-          borderBottom: 'solid 1px',
-          paddingBottom: '1rem',
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
         }}
       >
-        <NavLink to="/home">홈</NavLink> |{' '}
-        <NavLink to="/meetings">자유열람실</NavLink> |{' '}
-        <NavLink to="/studyrooms">스터디룸</NavLink> |{' '}
-        <NavLink to="/schedule">일정관리</NavLink> |{' '}
-        <NavLink to="/profile">프로필</NavLink> |{' '}
-        <NavLink to="/settings">마이페이지</NavLink> |{' '}
+        <NavLink to="/home">홈</NavLink> |{" "}
+        <NavLink to="/meetingrooms">자유열람실</NavLink> |{" "}
+        <NavLink to="/studyrooms">스터디룸</NavLink> |{" "}
+        <NavLink to="/schedules">일정관리</NavLink> |{" "}
+        <NavLink to="/profile">프로필</NavLink> |{" "}
+        <NavLink to="/settings">마이페이지</NavLink> |{" "}
       </nav>
       <Outlet />
     </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import App from './App';
-import Home from './pages/Home';
-import Studyrooms from './pages/Studyrooms';
-import Meetingrooms from './pages/Meetingrooms';
-import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import Schedule from './pages/Schedule';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import Studyrooms from "./pages/Studyrooms";
+import Meetingrooms from "./pages/Meetingrooms";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import Schedule from "./pages/Schedule";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,20 +15,14 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="home" element={<Home />} />
-          <Route
-            path="studyrooms"
-            element={<Studyrooms />}
-          />
-          <Route
-            path="meetings"
-            element={<Meetingrooms />}
-          />
-          <Route path="schedule" element={<Schedule />} />
+          <Route path="studyrooms" element={<Studyrooms />} />
+          <Route path="meetingrooms" element={<Meetingrooms />} />
+          <Route path="schedules" element={<Schedule />} />
           <Route path="profile" element={<Profile />} />
           <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
