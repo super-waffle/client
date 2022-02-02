48a7384fdb4d640aa52857decfd7f9c9efb5386e
eunyeong1113 <eylee1113@naver.com>
2022-02-02 19:20:59 +0900

    [#13] feat: openvidu connection

--- src/pages/Meetingrooms.js
+++ src/pages/Meetingrooms.js
@@ -1,7 +1,10 @@
+import VideoRoomComponent from '../components/meetingroom/VideoRoomComponent'
+
 export default function Meetingrooms() {
   return (
     <main style={{ padding: '1rem 0' }}>
       <h2>Meetingrooms</h2>
+      <VideoRoomComponent/>
     </main>
   );
 }
