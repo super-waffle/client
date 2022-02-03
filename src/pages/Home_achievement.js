760311da1be4179a4082c47f98f6f3595f66de5e
rosieyeon <sseyeon956@gmail.com>
2022-02-03 17:25:34 +0900

    [#43] feat: home/achievement wireframe

--- src/pages/Home_achievement.js
+++ src/pages/Home_achievement.js
@@ -1,20 +1,49 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react/cjs/react.development";
+import isLogin from "../utils/isLogin";
+import "../statics/css/homeAchievement.css";
 
 export default function HomeAchievement() {
-  console.log("achievement");
+  const TOKEN = localStorage.getItem("accessToken");
+
+  const [nickname, setNickname] = useState("");
+  const [level, setLevel] = useState("");
+  const [levelName, setLevelName] = useState("");
+  const [levelImg, setLevelImg] = useState("");
+  const [timeGoal, setTimeGoal] = useState("");
+  const [timeTotal, setTimeTotal] = useState("");
+
+  if (isLogin()) {
+    axios
+      .get("/users", {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      })
+      .then((res) => {
+        const USER = res.data.user;
+        setNickname(USER.userNickname);
+        setLevel(USER.userLevel.levelSeq);
+        setLevelName(USER.userLevel.levelName);
+        setLevelImg(USER.userImageLevel.levelImg);
+        setTimeGoal(USER.userTimeGoal);
+        setTimeTotal(USER.userTimeTotal);
+      });
+  }
   return (
-    <div>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
-      <h2>achievement</h2>
+    <div className="home-achievement">
+      <div className="home-achievement-top">
+        <span>나의 레벨 및 업적</span>
+        <div className="home-achievement-top-box">
+          <div className="home-achievement-level"></div>
+          <div className="home-achievement-achieve"></div>
+        </div>
+      </div>
+      <div className="home-achievement-bottom">
+        <span>{nickname}님의 업적도감</span>
+        <div className="home-achievement-dogam"></div>
+      </div>
     </div>
   );
 }
