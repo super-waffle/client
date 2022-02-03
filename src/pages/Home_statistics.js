243fdaa93edb0e213efefe7b5fddf99b994527f7
rosieyeon <sseyeon956@gmail.com>
2022-02-03 17:49:11 +0900

    [#44] feat: home/statistics wireframe

--- src/pages/Home_statistics.js
+++ src/pages/Home_statistics.js
@@ -1,7 +1,16 @@
+import "../statics/css/homeStatistics.css";
+
 export default function HomeStatistics() {
   return (
-    <div>
-      <h2>statistics</h2>
+    <div className="home-statistics">
+      <div className="home-statistics-top">
+        <span>365일의 공부 기록</span>
+        <div className="home-statistics-top-box"></div>
+      </div>
+      <div className="home-statistics-bottom">
+        <span>내가 목표시간을 달성했더라면...</span>
+        <div className="home-statistics-bottom-box"></div>
+      </div>
     </div>
   );
 }
