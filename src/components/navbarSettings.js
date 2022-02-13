441747299f4d678b7680bded294577db62638f1b
rosieyeon <sseyeon956@gmail.com>
2022-02-13 17:04:49 +0900

    [#67] feat: Add members list

--- src/components/navbarSettings.js
+++ src/components/navbarSettings.js
@@ -35,11 +35,20 @@ function NavbarSettings() {
         </div>
 
         <div className="navbar-settings-box__menus">
+          <NavLink className="navbar-settings-box__menus-menu" to={"/"}>
+            <div className="navbar-settings-box__menus-menu-box">
+              <div className="navbar-settings-box__menus-menu-box active-box"></div>
+              <span className="navbar-settings-box__menus-menu-box-content">
+                내 스터디 전체보기
+              </span>
+            </div>
+          </NavLink>
+
           <NavLink className="navbar-settings-box__menus-menu" to={"study"}>
             <div className="navbar-settings-box__menus-menu-box">
               <div className="navbar-settings-box__menus-menu-box active-box"></div>
               <span className="navbar-settings-box__menus-menu-box-content">
-                모집중 스터디
+                내가 개설한 스터디 관리
               </span>
             </div>
           </NavLink>
