01c621bcd16065cff425f89e69c17d60d8dd3b34
rosieyeon <sseyeon956@gmail.com>
2022-02-18 03:34:15 +0900

    [#123] feat: home/todays transition

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,19 +5,6 @@ export default function Footer() {
   const logo = "../../images/logo-400.png";
   return (
     <footer className="footer">
-      {/* <div className="footer__contents">
-        <div className="footer__logo">
-          <img src={logo} alt=""></img>
-        </div>
-
-        <div className="footer__introduce">
-          <div>서비스</div>
-          <div></div>
-        </div>
-
-        <div className="footer__madeby">Creator</div>
-        <div>lorem</div>
-      </div> */}
       <div className="footer__contents"></div>
 
       <div className="footer__rights">
